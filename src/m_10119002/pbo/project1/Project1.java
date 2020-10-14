5d763d8818b0d29c9a4fba163639d4e4290e6091
fsahi <fsahi@FS-DesktopPC>
2020-10-15 00:24:27 +0700

    New Project

---
+++ src/m_10119002/pbo/project1/Project1.java
@@ -0,0 +1,12 @@
+package m_10119002.pbo.project1;
+
+/**
+ *
+ * @author firman_10119002
+ */
+public class Project1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
