157811e121774bddaf0209ab447670e9200ebe7f
m10119002 <git@github.com>
2020-10-15 00:26:23 +0700

    Author and Commiter

--- src/m_10119002/pbo/project1/Project1.java
+++ src/m_10119002/pbo/project1/Project1.java
@@ -7,6 +7,6 @@ package m_10119002.pbo.project1;
 public class Project1 {
     
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 2");
     }
 }
